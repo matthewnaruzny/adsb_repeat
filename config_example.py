92a491619c42a0b99b051073ae67928057a743dd
Matthew Naruzny <matthewnaruzny@gmail.com>
2022-10-14 16:49:04 -0600

    Send data to MQTT

---
+++ config_example.py
@@ -0,0 +1,8 @@
+aws = {
+    "endpoint_addr": "ENDPOINT",
+    "endpoint_port": 8883,
+    "root_ca": "certs/root-CA.crt",
+    "cert": "certs/CERTIFICATE",
+    "private_key": "certs/PRIVATE_KEY",
+    "id": "ID"
+}
--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import os
 import time
 import json
 
-from tabulate import tabulate
+import config
 
-# from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+aws_config = config.aws
 
-# myMQTTClient = AWSIoTMQTTClient("adsb-repeater-0001")
-# myMQTTClient.configureEndpoint("YOUR.ENDPOINT", 8883)
-# myMQTTClient.configureCredentials("certs/root-CA.crt", "certs/adsb_remote_0001.private.key",
-#                                  "certs/adsb_remote_0001.cert.pem")
-# myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-# myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-# myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-# myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+myMQTTClient = AWSIoTMQTTClient(aws_config['id'])
+myMQTTClient.configureEndpoint(aws_config['endpoint_addr'], aws_config['endpoint_port'])
+myMQTTClient.configureCredentials(aws_config['root_ca'], aws_config['private_key'],
+                                  aws_config['cert'])
+myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
 os.chdir('/run/dump1090-mutability')
 
@@ -22,8 +24,12 @@ def recv_message(msg):
     print(msg)
 
 
-# myMQTTClient.connect()
-# myMQTTClient.subscribe("adsb/remote/0001", 1, recv_message)
+myMQTTClient.connect()
+myMQTTClient.subscribe("adsb/remote/" + aws_config['id'], 1, recv_message)
+
+repeat_num = aws_config['id'].split('_')[2]
+default_topic = "adsb/remote/" + repeat_num
+
 
 while True:
     os.system("clear")
@@ -31,14 +37,7 @@ while True:
     with open("aircraft.json", "r") as f:
         a = json.load(f)
         t_aircraft = a['aircraft']
-        for i in t_aircraft:
-            try:
-                current_data.append([i['hex'], i['flight'], i['squawk'], i['altitude'], i['speed'], i['lat'], i['lon']])
-            except KeyError:
-                current_data.append([i['hex']])
-
-        print("---------------Flight Data---------------\n",
-              tabulate(current_data, headers=["icao24", "Flight #", "Squawk", "Alt", "Speed", "lat", "long"]),
-              "\n-----------------------------------------")
+        myMQTTClient.publish(default_topic + "/tracking/num", len(t_aircraft), 1)
+        myMQTTClient.publish(default_topic + "/tracking", t_aircraft, 1)
 
     time.sleep(1)
