e267bb52a6388d5fe2925283c6ac147a67b6d6dd
Matthew Naruzny <matthewnaruzny@gmail.com>
2022-10-14 17:57:06 -0600

    Updated mqttclient

--- main.py
+++ main.py
@@ -22,18 +22,19 @@ myMQTTClient.configureEndpoint(aws_config['endpoint_addr'], aws_config['endpoint
 myMQTTClient.configureCredentials(root_path, private_path, cert_path)
 myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+myMQTTClient.configureConnectDisconnectTimeout(15)  # 15 sec
+myMQTTClient.configureMQTTOperationTimeout(15)  # 15 sec
 
 
 def recv_message(msg):
-    print(msg)
+    print("New Message: " + str(msg))
 
 
 print("Connecting...")
 myMQTTClient.connect()
 myMQTTClient.subscribe("adsb/" + aws_config['id'], 1, recv_message)
-print("Connected")
+print("adsb/" + aws_config['id'])
+print("Connected and Subscribed")
 
 default_topic = "adsb/" + aws_config['id']
 
@@ -48,8 +49,8 @@ while True:
         try:
             myMQTTClient.publish(default_topic + "/tracking/num", str(len(t_aircraft)), 1)
             myMQTTClient.publish(default_topic + "/tracking", str(t_aircraft), 1)
+            print("Published")
         except Exception:
             print("Unable to Publish")
-        print("Published")
 
-    time.sleep(1)
+    time.sleep(5)
