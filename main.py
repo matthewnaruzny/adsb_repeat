1a1886f161d94efd22799ffbeb8380af733306d0
Matthew Naruzny <matthewnaruzny@gmail.com>
2022-10-21 12:02:46 -0600

    Rewrote Object-Oriented

--- main.py
+++ main.py
@@ -5,52 +5,61 @@ import json
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 import config
-aws_config = config.aws
-
-private_path = os.path.abspath(aws_config['private_key'])
-cert_path = os.path.abspath(aws_config['cert'])
-root_path = os.path.abspath(aws_config['root_ca'])
-print(private_path)
-print(cert_path)
-print(root_path)
-
-print(config.aws)
-
-print("Setting Up...")
-myMQTTClient = AWSIoTMQTTClient(aws_config['id'])
-myMQTTClient.configureEndpoint(aws_config['endpoint_addr'], aws_config['endpoint_port'])
-myMQTTClient.configureCredentials(root_path, private_path, cert_path)
-myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myMQTTClient.configureConnectDisconnectTimeout(15)  # 15 sec
-myMQTTClient.configureMQTTOperationTimeout(15)  # 15 sec
 
 
-def recv_message(msg):
-    print("New Message: " + str(msg))
+class ADSB_Controller:
 
+    def __init__(self, aws_config):
+        self.aws_config = aws_config
+        self.mqtt_client = self.establish_aws_connection()
 
-print("Connecting...")
-myMQTTClient.connect()
-myMQTTClient.subscribe("adsb/" + aws_config['id'], 1, recv_message)
-print("adsb/" + aws_config['id'])
-print("Connected and Subscribed")
+        # Start Monitoring
+        self.monitor()
 
-default_topic = "adsb/" + aws_config['id']
+    def establish_aws_connection(self):
+        private_path = os.path.abspath(self.aws_config['private_key'])
+        cert_path = os.path.abspath(self.aws_config['cert'])
+        root_path = os.path.abspath(self.aws_config['root_ca'])
 
-os.chdir('/run/dump1090-mutability')
+        print("Configuring Client...")
+        myMQTTClient = AWSIoTMQTTClient(self.aws_config['id'])
+        myMQTTClient.configureEndpoint(self.aws_config['endpoint_addr'], self.aws_config['endpoint_port'])
+        myMQTTClient.configureCredentials(root_path, private_path, cert_path)
+        myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+        myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+        myMQTTClient.configureConnectDisconnectTimeout(15)  # 15 sec
+        myMQTTClient.configureMQTTOperationTimeout(15)  # 15 sec
 
-while True:
-    current_data = []
-    with open("aircraft.json", "r") as f:
-        a = json.load(f)
-        t_aircraft = a['aircraft']
-        print(t_aircraft)
+        print("Connecting to Endpoint...")
         try:
-            myMQTTClient.publish(default_topic + "/tracking/num", str(len(t_aircraft)), 1)
-            myMQTTClient.publish(default_topic + "/tracking", str(t_aircraft), 1)
-            print("Published")
+            myMQTTClient.connect()
+            myMQTTClient.subscribe("adsb/" + self.aws_config['id'], 1, self.recv_message)
+            print("adsb/" + self.aws_config['id'])
+            print("Connected and Subscribed")
         except Exception:
-            print("Unable to Publish")
+            print("Failed to Connect")
+            return
 
-    time.sleep(5)
+        return myMQTTClient
+
+    def recv_message(msg):
+        print("New Message: " + str(msg))
+
+    def monitor(self):
+        default_topic = "adsb/" + self.aws_config['id']
+        os.chdir('/run/dump1090-mutability')
+
+        while True:
+            current_data = []
+            with open("aircraft.json", "r") as f:
+                a = json.load(f)
+                t_aircraft = a['aircraft']
+                print(t_aircraft)
+                try:
+                    self.mqtt_client.publish(default_topic + "/tracking/num", str(len(t_aircraft)), 1)
+                    self.mqtt_client.publish(default_topic + "/tracking", str(t_aircraft), 1)
+                    print("Published")
+                except Exception:
+                    print("Unable to Publish")
+
+            time.sleep(5)
