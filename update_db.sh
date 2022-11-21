2ee574ce1cf14b4b96ddf07ed723ee5ec1c17c15
Matthew Naruzny <matthewnaruzny@gmail.com>
2022-11-21 16:46:49 -0700

    Modified DB Update Script

--- update_db.sh
+++ update_db.sh
@@ -1,4 +1,4 @@
-rm -rf indexedDB_old
 wget -O indexedDB_old.zip https://www.mictronics.de/aircraft-database/indexedDB_old.php
+rm -rf indexedDB_old
 unzip indexedDB_old.zip -d indexedDB_old
 rm indexedDB_old.zip
